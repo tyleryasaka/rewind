e635d4d205d80b043a630931193911f6ad6e295e
Tyler Yasaka <contact@tyleryasaka.me>
2019-10-18 23:46:00 -0700

    checkpoint

--- entropy.js
+++ entropy.js
@@ -1,3 +1,16 @@
+const { Graph, json, alg } = require("@dagrejs/graphlib")
+
+function count (counter, index, init = 1) {
+  counter[index] = counter[index] ? counter[index] + 1 : init
+}
+
+function collect (cabinet, index, item) {
+  cabinet[index] = cabinet[index] || []
+  if (!cabinet[index].includes(item)) {
+    cabinet[index].push(item)
+  }
+}
+
 function Relation (a, b, distance, rowLength) {
   if (a < b) {
     this.a = a
@@ -8,23 +21,26 @@ function Relation (a, b, distance, rowLength) {
     this.b = a
     this.distance = rowLength - distance
   }
-  this.id = `${this.a}-${this.b}-${this.distance}`
+  this.id = `${this.a}-${this.b}:${this.distance}`
 }
 
 function isRelationEquivalent (relation1, relation2) {
   return relation1.id === relation2.id
 }
 
-function allRelations (row, fullRowLength, relations = []) {
-  if (row.length === 0) {
-    return relations
+function allRelations (row, fullRowLength, index = 0, { relations = [], byPosition = {} } = {}) {
+  if (row.length === 1) {
+    return { relations, byPosition }
   }
   const head = row[0]
   const tail = row.slice(1)
   tail.forEach((tailItem, tailIndex) => {
-    relations.push(new Relation(head, tailItem, tailIndex + 1, fullRowLength))
+    const relation = new Relation(head, tailItem, tailIndex + 1, fullRowLength)
+    relations.push(relation)
+    collect(byPosition, String(index), relation.id)
+    collect(byPosition, String(index + tailIndex + 1), relation.id)
   })
-  return allRelations(tail, fullRowLength, relations)
+  return allRelations(tail, fullRowLength, index + 1, { relations, byPosition })
 }
 
 function duplicateRelations (relations, duplicates = {}) {
@@ -35,19 +51,24 @@ function duplicateRelations (relations, duplicates = {}) {
   const tail = relations.slice(1)
   tail.forEach(tailItem => {
     if (isRelationEquivalent(head, tailItem)) {
-      duplicates[head.id] = duplicates[head.id] ? duplicates[head.id] + 1 : 2
+      count(duplicates, head.id, 2)
     }
   })
   return duplicateRelations(tail, duplicates)
 }
 
 function allTokens (row) {
-  const relations = allRelations(row, row.length)
-  // relations.forEach(r => {
-  //   console.log(r.id)
-  // })
+  const { relations, byPosition } = allRelations(row, row.length)
   const duplicates = duplicateRelations(relations)
-  console.log(duplicates)
+  const duplicatesGraph = new Graph({ directed: true })
+  Object.keys(duplicates).forEach(relation => {
+    row.forEach((cellState, cellPosition) => {
+      if (byPosition[cellPosition].includes(relation)) {
+        duplicatesGraph.setEdge(`cell:${cellPosition}`, `rel:${relation}`)
+      }
+    })
+  })
+  console.log(alg.tarjan(duplicatesGraph))
 }
 
 function allRecursions (row) {
--- tests/entropy-test.js
+++ tests/entropy-test.js
@@ -4,10 +4,11 @@ const {
   isRelationEquivalent
 } = require('../entropy')
 
-const row = [1,2,1,4,1,2]
+const row = [1, 2, 1, 4, 1, 2]
+// const row = [1, 2, 4, 4, 1, 2, 4]
 entropy(row)
 
 const relation1 = new Relation(5, 4, 1, 5)
 const relation2 = new Relation(4, 5, 4, 5)
 // console.log(relation1.id, relation2.id)
-console.log(isRelationEquivalent(relation1, relation2))
+// console.log(isRelationEquivalent(relation1, relation2))
